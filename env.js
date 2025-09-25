c81568c2bb54e94998c66a5b518a22bb3290d175
emanuel.ferreira <emanuel2005batista@gmail.com>
2025-09-24 21:30:44 -0300

    muda env

--- env.js
+++ env.js
@@ -2,10 +2,3 @@ window.env = {
   //API_URL: "http://127.0.0.1:5000"
   API_URL: "https://emanuelferreira.pythonanywhere.com"
 };
-
-const isGithub = location.hostname.includes("github.io");
-if (isGithub) {
-  const base = document.createElement("base");
-  base.href = "/blog-web/";
-  document.head.appendChild(base);
-}
